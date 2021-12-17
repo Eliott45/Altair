85e465a7abe5d6c3be14e3a8d1e2e7e09b649bc9
Maksim <eliott_nu@mail.ru>
2021-12-18 02:38:47 +0900

    Player movement anim (idle, run)

--- Altair/Assets/Scripts/Player.cs
+++ Altair/Assets/Scripts/Player.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,15 +6,47 @@ public class Player : MonoBehaviour
     [Header("Header set in Inspector: ")]
     [SerializeField] private Camera _camera;
     [SerializeField] private NavMeshAgent _navMeshAgent;
+    [SerializeField] private Animator _animator;
+
+    private static readonly int Move = Animator.StringToHash("Move");
+    
+    private Vector2 _smoothDeltaPosition = Vector2.zero;
+    private Vector2 _velocity = Vector2.zero;
+
+    private void Start()
+    {
+        _navMeshAgent.updatePosition = false;
+    }
 
     private void Update()
     {
+        var worldDeltaPosition = _navMeshAgent.nextPosition - transform.position;
+        
+        // Map 'worldDeltaPosition' to local space
+        var dx = Vector3.Dot (transform.right, worldDeltaPosition);
+        var dy = Vector3.Dot (transform.forward, worldDeltaPosition);
+        var deltaPosition = new Vector2 (dx, dy);
+
+        // Low-pass filter the deltaMove
+        var smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
+        _smoothDeltaPosition = Vector2.Lerp (_smoothDeltaPosition, deltaPosition, smooth);
+        
+        // Update velocity if delta time is safe
+        if (Time.deltaTime > 1e-5f)
+            _velocity = _smoothDeltaPosition / Time.deltaTime;
+
+        var moving = _velocity.magnitude > 0f && _navMeshAgent.remainingDistance > (_navMeshAgent.radius / 3);
+
+        _animator.SetBool(Move, moving);
+
         if (!Input.GetMouseButtonDown(0)) return;
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out var hit))
-        {
-            _navMeshAgent.velocity = Vector3.zero;
-            _navMeshAgent.SetDestination(hit.point);
-        }
+        if (!Physics.Raycast(ray, out var hit)) return;
+        _navMeshAgent.velocity = Vector3.zero;
+        _navMeshAgent.SetDestination(hit.point);
+    }
+
+    public void OnAnimatorMove () {
+        transform.position = _navMeshAgent.nextPosition;  // Update postion to agent position
     }
 }
