64f60902e002ecfb76df1960d2afb24971a29361
Maksim <eliott_nu@mail.ru>
2021-12-20 14:36:54 +0900

    Player have different speed when walking/run

--- Altair/Assets/Scripts/Player.cs
+++ Altair/Assets/Scripts/Player.cs
@@ -8,15 +8,46 @@ public class Player : MonoBehaviour
     [SerializeField] private Camera _camera;
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Animator _animator;
+    [SerializeField] private float _runSpeed = 5f; 
+    [SerializeField] private float _walkSpeed = 2.5f; 
 
     private Vector2 _smoothDeltaPosition = Vector2.zero;
     private Vector2 _velocity = Vector2.zero;
-    private static readonly int Move = Animator.StringToHash("Move");
+    private static readonly int Moving = Animator.StringToHash("Move");
     private static readonly int Distance = Animator.StringToHash("Distance");
 
     private void Start() => _agent.updatePosition = false;
 
     private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Move();
+        if (_agent.hasPath) AnimateMove();
+    }
+
+    /// <summary>
+    /// Update position to agent position.
+    /// </summary>
+    public void OnAnimatorMove () => transform.position = _agent.nextPosition;
+
+    private void Move()
+    {
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (!Physics.Raycast(ray, out var hit)) return;
+        _agent.SetDestination(hit.point);
+        
+        var position = _agent.transform.position;
+        var distance = Math.Abs((position - hit.point).x) > 3f || Math.Abs((position - hit.point).z) > 2.5f;
+        
+        _animator.SetFloat(Distance, Convert.ToInt32(distance));
+
+        _agent.speed = distance ? _runSpeed : _walkSpeed;
+    }
+
+    /// <summary>
+    /// Animate character when moving.
+    /// </summary>
+    private void AnimateMove()
     {
         var worldDeltaPosition = _agent.nextPosition - transform.position;
         
@@ -30,27 +61,10 @@ public class Player : MonoBehaviour
         _smoothDeltaPosition = Vector2.Lerp (_smoothDeltaPosition, deltaPosition, smooth);
         
         // Update velocity if delta time is safe
-        if (Time.deltaTime > 1e-5f)
-            _velocity = _smoothDeltaPosition / Time.deltaTime;
-
+        if (Time.deltaTime > 1e-5f) _velocity = _smoothDeltaPosition / Time.deltaTime;
+        
         var moving = _velocity.magnitude > 0f && _agent.remainingDistance > (_agent.radius / 3);
-
-        _animator.SetBool(Move, moving);
-
-        if (!Input.GetMouseButtonDown(0)) return;
-        var ray = _camera.ScreenPointToRay(Input.mousePosition);
-        
-        if (!Physics.Raycast(ray, out var hit)) return;
-        _agent.SetDestination(hit.point);
-        
-        var position = _agent.transform.position;
-        _animator.SetFloat(Distance, Math.Abs((position - hit.point).x) > 3f || Math.Abs((position - hit.point).z) > 2.5f
-            ? 1 : 0);
+        _animator.SetBool(Moving, moving);
     }
-
-    /// <summary>
-    /// Update position to agent position.
-    /// </summary>
-    public void OnAnimatorMove () => transform.position = _agent.nextPosition;  
     
 }
